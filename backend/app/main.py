ae39a4909c25fe5ec0dd348b4ca32c0ccbc93768
Hugo Blanco <hugoblancodemelo@gmail.com>
2025-09-21 19:12:28 +0200

    feat: Adds container stats endpoint
    Adds an endpoint to retrieve statistics for a specific container,
    including CPU usage, memory usage, and network I/O.
    
    Handles potential API errors from the Docker client gracefully.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+import docker.errors
 from fastapi import FastAPI
 import docker
 
@@ -23,6 +24,21 @@ async def get_containers():
         container_list.append(container_info)
     
     return {"containers": container_list}
+
+@app.get("/containers/{container_id}/stats")
+async def get_stats(container_id: str):
+    try:
+        container = await get_container_by_id(container_id)
+        stats = container.stats(stream=False)
+        return {
+                "cpu_usage": stats["cpu_stats"]["cpu_usage"]["total_usage"],
+                "memory_usage": stats["memory_stats"]["usage"],
+                "memory_limit": stats["memory_stats"]["limit"],
+                "network_rx": stats["networks"]["eth0"]["rx_bytes"] if "networks" in stats else 0,
+                "network_tx": stats["networks"]["eth0"]["tx_bytes"] if "networks" in stats else 0
+            }
+    except (docker.errors.APIError):
+        return{"Internal Error"}
 
 @app.post("/containers/{container_id}/restart")
 async def restart_container_from_id(container_id: str):
