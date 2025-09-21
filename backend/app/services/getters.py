1137d235388c47857abda77424a9349e66cf62a3
Hugo Blanco <hugoblancodemelo@gmail.com>
2025-09-21 19:11:55 +0200

    fix: Handles container retrieval errors gracefully

--- backend/app/services/getters.py
+++ backend/app/services/getters.py
@@ -3,5 +3,9 @@ import docker
 docker_cli = docker.from_env()
 
 async def get_container_by_id(container_id: str):
-    container = docker_cli.containers.get(container_id)
-    return container
+    try:
+        container = docker_cli.containers.get(container_id)
+        print("Container found")
+        return container
+    except Exception as e:
+        return{"Error {e}"}
